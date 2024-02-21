2c33097c4e989776bc1f548e817c07b276be2120
JuanDCespedes <jdcu2005@gmail.com>
2024-02-21 07:53:12 -0500

    act

--- cartas.py
+++ cartas.py
@@ -1,3 +1,4 @@
+import random
 class Carta:
     def __init__(self, valor, pinta):
         self.valor=valor
@@ -12,24 +13,43 @@ class Carta:
         return self.valor + " de "+ self.pinta
 class Mazo:
     def __init__(self, jugador =False):
+        
         if jugador:
             self.cartas=[]
         else:
             self.cartas=[Carta(v, p)for v in ["A","J", "Q", "K"]+[str(x) for x in range(2, 11)] for p in ["picas", "treboles", "corazones", "diamantes"]]
+            random.shuffle(self.cartas)   
     def dar_valor(self):
         valor=0
         for c in self.cartas:
             valor+=c.dar_valor()
+        if self.tiene_as() and valor <=11:
+            valor+=10
         return valor
+    def tiene_as(self):
+        for c in self.cartas:
+            if c.valor=="A":
+                return True
+        return False
     def dar_carta(self):
         return self.cartas.pop()
     def agregar_carta(self,carta):
         self.cartas.append(carta)
+    def mostrar_cartas(self, todas=False):
+        if todas:
+            print(self.cartas[0].mostrar())
+        else:
+            print("* de *")
+        for c in self.cartas[1:]:
+            print(c.mostrar())
+    
 if __name__=="__main__":
     m=Mazo()
     j=Mazo(True)
     j.agregar_carta(m.dar_carta())
     j.cartas.append(m.dar_carta())
+    j.agregar_carta(Carta("A", "picas"))
+    j.mostrar_cartas()
     for c in j.cartas:
         print(c.mostrar())
     print(j.dar_valor())
---
+++ juego.py
@@ -0,0 +1,44 @@
+from cartas import *
+class Juego:
+    def __init__(self):
+        self.mazo=Mazo()
+        self.casa=Mazo(True)
+        self.jugador=Mazo(True)
+    def iniciar_juego(self):
+        self.casa.agregar_carta(self.mazo.dar_carta())
+        self.casa.agregar_carta(self.mazo.dar_carta())
+        self.jugador.agregar_carta(self.mazo.dar_carta())
+        self.jugador.agregar_carta(self.mazo.dar_carta())
+    def mostrar_juego(self):
+        print("jugador:")
+        self.jugador.mostrar_cartas()
+        print("casa:")
+        self.casa.mostrar_cartas() 
+    def pedir(self, pide=True):
+        while pide:
+            turn=input("quieres otra carta? (s/n)\n")
+            if turn == "s":
+                self.jugador.agregar_carta(self.mazo.dar_carta())
+                self.jugador.mostrar_cartas()
+                print(self.jugador.dar_valor())
+                if self.jugador.dar_valor() > 21:
+                    print("una pena, te pasas, perdiste xd")
+                    print("la casa gano")
+                    break
+            else:
+                self.jugador.mostrar_cartas(True)
+                print(self.jugador.dar_valor())
+                int(self.jugador.dar_valor())
+                if self.jugador.dar_valor() <= 21:
+                    while self.casa.dar_valor() < self.jugador.dar_valor():
+                        self.casa.agregar_carta(self.mazo.dar_carta())
+                        self.casa.mostrar_cartas()
+                        print(self.casa.dar_valor())
+                        if self.casa.dar_valor() >= self.jugador.dar_valor and not(self.casa.dar_valor > 21):
+                            print("la casa gana xd, tuviste: "+ self.jugador.dar_valor())
+                            print("mientras que la casa: "+self.casa.dar_valor())
+                elif self.jugador.dar_valor() > 21:
+                    print("una pena, te pasas, perdiste xd")
+                    print("la casa gano")
+                    break
+                break
