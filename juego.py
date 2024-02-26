51294bb30e0de0a812fdae5e6ead7bd162513d0f
JuanDCespedes <159020969+JuanDCespedes@users.noreply.github.com>
2024-02-25 19:40:46 -0500

    Update juego.py

--- juego.py
+++ juego.py
@@ -50,3 +50,8 @@ class Juego:
                     print("la casa gano")
                     break;
                 break;
+if __name__ == "__main__":
+    juego = Juego()
+    juego.iniciar_juego()
+    juego.mostrar_juego()
+    juego.pedir()
